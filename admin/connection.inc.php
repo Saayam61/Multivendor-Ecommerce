6ea125d3102afe1de773927339a081f99bde5d45
Saayam61 <97223984+Saayam61@users.noreply.github.com>
2024-04-13 13:46:17 +0545

    git commit

---
+++ admin/connection.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "ecom";
+
+$con = mysqli_connect($servername, $username, $password, $database);
+if($con){
+    echo "Database Connection Success";
+}
+else{
+    mysqli_connect_error($con);
+}
+
+?>
---
+++ admin/functions.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+function pr($arr){
+    echo '<pre>';
+    print_r($arr);
+}
+
+function prx($arr){
+    echo '<pre>';
+    print_r($arr);
+    die();
+}
+
+?>
